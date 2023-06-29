3341105572d0cc4011f44746530371ce450b4224
Raphael-M-Assis <raphaelmoreira120@gmail.com>
2023-06-29 12:50:11 -0300

    adicao de prints para demonstrar o processo de criacao dos genes filhos

--- Principal.py
+++ Principal.py
@@ -1,4 +1,11 @@
 from ProblemaMochila import ProblemaMochila
 
-problema = ProblemaMochila(8, 8, 0.05, 5000, 8) #(numeroIndividuos, numeroGenes, taxaMutacao, capacidadeMochila, condicaoParada):
-problema.inicializa()
+class Principal:
+    def __init__(self):
+        pass
+
+    def main(self):
+        problema = ProblemaMochila(8, 8, 0.05, 5000, 8) #(numeroIndividuos, numeroGenes, taxaMutacao, capacidadeMochila, condicaoParada):
+        problema.inicializa()
+
+Principal().main()
